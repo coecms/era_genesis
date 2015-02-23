a05b0e7c2455a85c5e642bf2ffabfa98f828332b
Holger Wolff <holger.wolff@monash.edu>
2015-02-23 15:43:15 +1100

    moved several routines around

--- era_genesis.py
+++ era_genesis.py
@@ -129,6 +129,93 @@ def read_all_data(args):
 
 
 
+def cleanup_args(args, base):
+    """(Namelist, dict of dict) -> Namelist
+
+    Converts start- and end time to datetime.datetime, calculates endtime if num is given,
+    and end_time, if num is given. Can read start and end time from base config file, but
+    options in args are gived priority.
+
+    """
+
+    def read_date_from_args(s):
+        """(str) -> datetime
+
+        reads the string s and tries to interpret it as datetime.
+
+        >>> read_date_from_args('20150101')
+        datetime.datetime(2015, 1, 1, 0, 0)
+        >>> read_date_from_args('2015013104')
+        datetime.datetime(2015, 1, 31, 4, 0)
+        >>> read_date_from_args('201512011231')
+        datetime.datetime(2015, 12, 1, 12, 31)
+        """
+
+        if len(s) == 8:
+            return_date = datetime.datetime.strptime(s, '%Y%m%d')
+        elif len(s) == 10:
+            return_date = datetime.datetime.strptime(s, '%Y%m%d%H')
+        elif len(s) == 12:
+            return_date = datetime.datetime.strptime(s, '%Y%m%d%H%M')
+        else:
+            raise ValueError("Can't read datetime: {}".format(s))
+
+        return return_date
+
+    stop = False
+    # See whether there are any parameters in the base configuration file
+    # that we might use.
+    base_knows_sdate = 'time' in base.keys() and 'sdate' in base['time'].keys()
+    base_knows_edate = 'time' in base.keys() and 'edate' in base['time'].keys()
+    if not args.lon:
+        print( "Need longitude" )
+        stop = True
+    if not args.lat:
+        print( "Need latitude" )
+        stop = True
+    if not args.start_date:
+        if base_knows_sdate:
+            args.start_date = '{:08}{:02}'.format(base['time']['sdate'], base['time']['shour'])
+        else:
+            print( "Need start date" )
+            stop = True
+    if not (args.end_date or args.num):
+        if base_knows_edate:
+            args.end_date = '{:08}{:02}'.format(base['time']['edate'], base['time']['ehour'])
+        else:
+            print ( "Need end date or number" )
+            stop = True
+
+    if stop:
+        print( " Please see help file for details (-h) ")
+        exit()
+
+    if ':' in args.intervall:
+        intervall_hours, intervall_minutes = args.intervall.split(':')
+    else:
+        intervall_hours = int(args.intervall)
+        intervall_minutes = '00'
+    args.intervall = datetime.timedelta(hours=int(intervall_hours), minutes=int(intervall_minutes))
+
+    args.start_date = read_date_from_args( args.start_date )
+
+    if args.end_date:
+        args.end_date = read_date_from_args( args.end_date )
+        args.num = 0
+        while args.start_date + args.num*args.intervall <= args.end_date:
+            args.num += 1
+    else:
+        if args.num:
+            args.end_date = (args.num - 1) * args.intervall + args.start_date
+        else:
+            raise ValueError( "Need either end date or num")
+    if args.lat < -90. or args.lat > 90.:
+        raise ValueError( "Latitude out of bounds: {}".format(args.lat))
+    if args.lon < 0. or args.lon > 360.:
+        raise ValueError( "Longitude out of bounds: {}".format(args.lon))
+
+
+    return args
 
 
 def parse_arguments():
@@ -139,64 +226,6 @@ def parse_arguments():
 
     import genesis_helpers as helpers
 
-    def cleanup_args(args):
-        """(Namelist) -> Namelist
-
-        Converts start- and end time to datetime.datetime, calculates endtime if num is given,
-        and end_time, if num is given.
-
-        """
-
-        def read_date_from_args(s):
-            """(str) -> datetime
-
-            reads the string s and tries to interpret it as datetime.
-
-            >>> read_date_from_args('20150101')
-            datetime.datetime(2015, 1, 1, 0, 0)
-            >>> read_date_from_args('2015013104')
-            datetime.datetime(2015, 1, 31, 4, 0)
-            >>> read_date_from_args('201512011231')
-            datetime.datetime(2015, 12, 1, 12, 31)
-            """
-
-            if len(s) == 8:
-                return_date = datetime.datetime.strptime(s, '%Y%m%d')
-            elif len(s) == 10:
-                return_date = datetime.datetime.strptime(s, '%Y%m%d%H')
-            elif len(s) == 12:
-                return_date = datetime.datetime.strptime(s, '%Y%m%d%H%M')
-            else:
-                raise ValueError("Can't read datetime: {}".format(s))
-
-            return return_date
-
-        if ':' in args.intervall:
-            intervall_hours, intervall_minutes = args.intervall.split(':')
-        else:
-            intervall_hours = int(args.intervall)
-            intervall_minutes = '00'
-        args.intervall = datetime.timedelta(hours=int(intervall_hours), minutes=int(intervall_minutes))
-
-        args.start_date = read_date_from_args( args.start_date )
-
-        if args.end_date:
-            args.end_date = read_date_from_args( args.end_date )
-            args.num = 0
-            while args.start_date + args.num*args.intervall <= args.end_date:
-                args.num += 1
-        else:
-            if args.num:
-                args.end_date = (args.num - 1) * args.intervall + args.start_date
-            else:
-                raise ValueError( "Need either end date or num")
-        if args.lat < -90. or args.lat > 90.:
-            raise ValueError( "Latitude out of bounds: {}".format(args.lat))
-        if args.lon < 0. or args.lon > 360.:
-            raise ValueError( "Longitude out of bounds: {}".format(args.lon))
-
-
-        return args
 
     parser = argparse.ArgumentParser(description='Cleans up the template file')
     parser.add_argument('-X', '--lon', help='longitude', type=float) #, required=True)
@@ -231,53 +260,39 @@ def parse_arguments():
 
     args = parser.parse_args()
 
-    if args.test:
-        import doctest
-        doctest.testmod()
-        exit()
-
-    if os.path.isfile(args.base):
-        base = f90nml.read(args.base)
-        base_knows_sdate = 'time' in base.keys() and 'sdate' in base['time'].keys()
-        base_knows_edate = 'time' in base.keys() and 'edate' in base['time'].keys()
-    else:
-        base = None
-
     setattr( args, 'intervall', '06:00')
 
-    stop = False
-    if not args.lon:
-        print( "Need longitude" )
-        stop = True
-    if not args.lat:
-        print( "Need latitude" )
-        stop = True
-    if not args.start_date:
-        if base_knows_sdate:
-            args.start_date = '{:08}{:02}'.format(base['time']['sdate'], base['time']['shour'])
-        else:
-            print( "Need start date" )
-            stop = True
-    if not (args.end_date or args.num):
-        if base_knows_edate:
-            args.end_date = '{:08}{:02}'.format(base['time']['edate'], base['time']['ehour'])
-        else:
-            print ( "Need end date or number" )
-            stop = True
-
-    if stop:
-        parser.print_help()
-        exit()
-
-    args = cleanup_args(args)
     return args
 
 def main():
     args = parse_arguments()
 
+    # Run unit Tests if required
+    if args.test:
+        import doctest
+        doctest.testmod()
+        exit()
+
+    # Read the base configuration file
+    if os.path.isfile(args.base):
+        base = f90nml.read(args.base)
+
+    else:
+        print( "Base configuration file not found: {}".format(args.base) )
+        exit(1)
+
+    args = cleanup_args(args, base)
+
     if args.debug:
+        print( " Read from command line arguments, and cleaned up ")
         for k in args.__dict__:
-            print( "{:10}: {}".format(k, args.__dict__[k]) )
+            print( "{:12}: {}".format(k, args.__dict__[k]) )
+
+        print( "\n Read from base configuration file." )
+        for k in base.keys():
+            print( '{}'.format(k) )
+            for kk in base[k].keys():
+              print( '     {:12}: {}'.format(kk, base[k][kk]))
 
     read_all_data(args)
 
